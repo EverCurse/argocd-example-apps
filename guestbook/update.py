c13f2c6de888433b39eed0cf151f4162b2057a7c
Your Name <you@example.com>
2022-03-08 17:44:11 +0800

    [p3][wallet]change to nginx

---
+++ guestbook/update.py
@@ -0,0 +1,8 @@
+import yaml
+file = open('guestbook-ui-deployment.yaml', 'r')
+file_data = file.read()
+file.close()
+data = yaml.load(file_data)
+data['spec']['template']['spec']['containers'][0]['image'] = "nginx"
+with open('guestbook-ui-deployment.yaml', 'w') as stream:
+  yaml.dump(data,stream)
